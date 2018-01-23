e7836b4f6fd63e5a5f9d5c64d8b187b7df7006f6
Sam Cordingley <sam@cordandberg.com>
2018-01-23 20:38:57 +1100

    session timer

--- src/components/Session.js
+++ src/components/Session.js
@@ -1,20 +1,29 @@
 import React, { PureComponent as Component } from 'react';
+import Timer from 'react.timer';
+
+const handleFinish = function () {
+  console.log('Skynet has become self-aware!');
+}
 
 class Session extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  componentDidMount() {
-  setTimeout(this.props.hide, 3000)
-  console.log("session did mount");
+  componentDidMount () {
+    setTimeout(this.props.hide, 3005)
+    console.log("session did mount");
   }
 
 
   render() {
     return (
       <div id="session">
-        <h1 id="sessionHeading">Your Session Has Started</h1>
+        <h1 id="sessionHeading">Loading Your Session</h1>
         <p id="sessionSubHeading">Dont freak out. Everything (here) is cool, but also warm.</p>
+        <h1 id="timer"><Timer countDown startTime={3.0} /></h1>
       </div>
-  
+
     );
   }
 }
