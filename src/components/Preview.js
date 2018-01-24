223397524b67cc628f21c59801b8d5638f2ce4aa
Sam Cordingley <sam@cordandberg.com>
2018-01-24 13:36:16 +1100

    saving users session name into local storage

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,12 +2,27 @@ import React, { PureComponent as Component } from 'react';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 
+
 class Preview extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      date: '',
+      title: ''
+    }
     this._changeScreen = this._changeScreen.bind(this);
     this._exitPopUp = this._exitPopUp.bind(this);
+    this._handleChange = this._handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    var currentDate = require("current-date");
+    var todaysDate = currentDate('date');
+    this.setState({date: todaysDate})
+
+    if(localStorage.getItem('docname')){
+      this.setState({title: localStorage.getItem('docname')})
+    }
   }
 
   _changeScreen() {
@@ -19,7 +34,7 @@ class Preview extends Component {
     html2canvas(input).then((canvas) => {
     const imgData = canvas.toDataURL('image/png');
     const pdf = new jsPDF();
-    pdf.addImage(imgData, 'JPEG', 10, 10, 180, 120);
+    pdf.addImage(imgData, 'JPEG', 10, 10, 180, 110);
     pdf.save("My_Brand_Session.pdf");
   });
   }
@@ -27,6 +42,15 @@ class Preview extends Component {
   _exitPopUp() {
     this.props.exitScreenOpen()
   }
+
+  _handleChange(e) {
+    this.setState({title: e.target.value}, function() {
+      localStorage['docname'] = this.state.title;
+    })
+  }
+
+
+
 
   render() {
 
@@ -37,15 +61,20 @@ class Preview extends Component {
         <button className="exitButton" onClick={this._exitPopUp}><div id="exit">Exit</div></button>
         <button className="stepsButton" onClick={this._changeScreen}><div id="steps">Back To Steps</div></button>
         <h1 className="previewMainHeading">Here lies your handy work</h1>
-        <h1 className="previewSubHeading">ADD KEYWORDS TO A PREVIOUS STEP AT ANY TIME</h1>
+        <h1 className="previewSubHeading">Add keywords to a previous step at any time.</h1>
+        <div id="sessionNameContainer">
+          <h1 id="sessionNameHeading">Give your session a name:</h1>
+            <input type="search" className="sessionNameInputField" placeholder="Eg. Your working title" onChange={this._handleChange} value={this.state.title}/>
+        </div>
+
         <div className="mb5">
-          <button id="previewPrintSessionButton" onClick={this.printDocument}>Print Session Notes <i class="zmdi zmdi-download"></i></button>
+          <button id="previewPrintSessionButton" onClick={this.printDocument}>Print Session Notes <i className="zmdi zmdi-download"></i></button>
         </div>
         <div id="divToPrint" className="mt4" style={{
           "backgroundColor": '#ffffff',
           "border": '10px solid black',
           "color": "black",
-          "max-width": '210mm',
+          "maxWidth": '210mm',
           "minHeight": '50mm',
           "padding": '20px',
           "marginLeft": 'auto',
@@ -56,8 +85,8 @@ class Preview extends Component {
 
         <div>
           <h1 className="docHeading">Magic Beans</h1>
-          <h1 className="docSubHeadings">Brand Session Date:</h1>
-          <h1 className="docSubHeadings">Working Title: </h1>
+          <h1 className="docSubHeadings">Brand Session Date: {this.state.date}</h1>
+          <h1 className="docSubHeadings">Working Title: {this.state.title}</h1>
           <h3>Brand Identity</h3>{this.props.identity.join(', ')}</div>
           <br />
           <div><h3>Audience</h3>{this.props.audience.join(', ')}</div>
